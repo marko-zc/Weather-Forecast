701640696dc4c1326085adf554a925fd37a6fcd5
marko-zc <marko.zarkovic.cerneli@gmail.com>
2021-03-07 23:41:19 +0100

    make current weather the home page

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ class App extends React.Component
           </ul>
         </nav>
         <Switch>
-          <Route path="/weather/current" exact>
+          <Route path={"/weather/current" | "/Weather-Forecast"}>
             <Current currentData={this.state.currentWeather} />
           </Route>
           <Route path="/weather/tomorrow">
