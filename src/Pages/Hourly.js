057851fb59dbf1ab532da06b18cd266d57d0f068
marko-zc <marko.zarkovic.cerneli@gmail.com>
2021-03-12 10:27:41 +0100

    update month in hourly forecast

--- src/Pages/Hourly.js
+++ src/Pages/Hourly.js
@@ -20,7 +20,7 @@ class Hourly extends React.Component
             ).map((hourlyData, i) => {
                 const time = new Date(hourlyData.time_epoch*1000)
               return <ul key={i} className="container">
-                <li><span className="date">{time.getDate() + ".0" + time.getMonth() + "."}</span> <br /> {time.getHours() + ':00'}</li>
+                <li><span className="date">{time.getDate() + ".0" + time.getMonth() + 1 + "."}</span> <br /> {time.getHours() + ':00'}</li>
                 <li><img src={hourlyData.condition.icon} /></li>
                 <li>{hourlyData.condition.text}</li>
                 <li className="temp">{hourlyData.temp_c}°C</li>
